90d827f398b3940d8b1c5b1933fc741fc5d0c76e
YC <shriyogeshchandra@gmail.com>
2019-07-31 11:12:49 +0530

    updates to email_helper, mainly changing sender address to mail@jochoice.com

--- src/helpers/email_helper.erl
+++ src/helpers/email_helper.erl
@@ -1,9 +1,48 @@
 -module(email_helper).
+%-export([send_website_message/3, send_pw_reset_email/3, send_signup_token_email/4, send_welcome_email/2, send_website_message_autoreply/2]).
 -export([send_website_message/3, send_pw_reset_email/3, send_signup_token_email/4, send_website_message_autoreply/2]).
 
 %send email
 send(Sender, ReceiverEmail, Message) ->
-	gen_smtp_client:send({Sender, [ReceiverEmail], Message}, [{relay, "smtp.gmail.com"}, {tls, always}, {port, 587}, {username, "arunanda1985@gmail.com"}, {password, "Dakini1234"}]).
+	gen_smtp_client:send({Sender, [ReceiverEmail], Message}, [{relay, "smtp.zoho.com"}, {tls, always}, {port, 587}, {username, "mail@jochoice.com"}, {password, "4runN$nd$"}]).
+
+
+%user welcome email - after they set their password. 
+%TODO
+%send_welcome_email(Login, Id) ->
+%	UserEmail = binary:bin_to_list(Login),
+%	{EmailHeaders, EmailBody} = build_welcome_email(TokenValue, Id, UserEmail),
+%	EmailContent = string:concat(EmailHeaders, EmailBody),
+%	Sender = "JoChoice Messenger",
+%	send(Sender, UserEmail, EmailContent).
+%
+%% create email body to be sent after receiving pw reset request
+%% TODO
+%build_welcome_email(TokenValue, Id, UserEmail) -> 
+%	Domain = "https://jochoice.com/",
+%	ResetRoute = "password/",
+%	TokenString = binary:bin_to_list(TokenValue),
+%	ResetLink = general_helpers:list_merge_no_sort([Domain, ResetRoute, TokenString]),
+%	UserName = binary:bin_to_list(db_helpers:name_given_id(Id)),
+%	EmailBody1 = "Hi ",
+%	EmailBody2 = "So you forgot your password, like we all sometimes do.." ,
+%	EmailBody3 = "It is easy to reset, just click the link below and enter the new password",
+%	EmailBody4 = "We look forward to seeing you soon at JoChoice.." ,
+%	NewLine = "\n",
+%	EmailBody5 = "Cheers!",
+%	EmailBody = general_helpers:list_merge_no_sort([EmailBody1, UserName, ",", NewLine, NewLine, EmailBody2, NewLine, NewLine, EmailBody3, NewLine, ResetLink, NewLine, NewLine, EmailBody4, NewLine, NewLine, EmailBody5]),
+%	%erlang:display(EmailBody),
+%	EmailHeader1 = "Subject: Jochoice Password Reset \r\nFrom: JoChoice Messenger <mail@jochoice.com>\r\nTo: ",
+%	EmailHeader2 = UserName,
+%	EmailHeader3 = "<",
+%	EmailHeader4 = UserEmail,
+%	EmailHeader5 = ">\r\n\r\n",
+%	EmailHeaders = general_helpers:list_merge_no_sort([EmailHeader1, EmailHeader2, EmailHeader3, EmailHeader4, EmailHeader5]),
+%	%erlang:display(EmailHeaders),
+%	{EmailHeaders, EmailBody}.
+%
+
+
 
 %password reset emails
 %send the password reset token email
@@ -11,7 +50,7 @@ send_pw_reset_email(TokenValue, Id, Login) ->
 	UserEmail = binary:bin_to_list(Login),
 	{EmailHeaders, EmailBody} = build_pw_reset_email(TokenValue, Id, UserEmail),
 	EmailContent = string:concat(EmailHeaders, EmailBody),
-	Sender = "JoChoice Bouncer",
+	Sender = "JoChoice Messenger",
 	send(Sender, UserEmail, EmailContent).
 
 % create email body to be sent after receiving pw reset request
@@ -29,7 +68,7 @@ build_pw_reset_email(TokenValue, Id, UserEmail) ->
 	EmailBody5 = "Cheers!",
 	EmailBody = general_helpers:list_merge_no_sort([EmailBody1, UserName, ",", NewLine, NewLine, EmailBody2, NewLine, NewLine, EmailBody3, NewLine, ResetLink, NewLine, NewLine, EmailBody4, NewLine, NewLine, EmailBody5]),
 	%erlang:display(EmailBody),
-	EmailHeader1 = "Subject: Jochoice Password Reset \r\nFrom: JoChoice Bouncer <arunanda1985@gmail.com>\r\nTo: ",
+	EmailHeader1 = "Subject: Jochoice Password Reset \r\nFrom: JoChoice Messenger <mail@jochoice.com>\r\nTo: ",
 	EmailHeader2 = UserName,
 	EmailHeader3 = "<",
 	EmailHeader4 = UserEmail,
@@ -58,14 +97,14 @@ build_signup_token_email(TokenValue, _Id, UserEmail, Name) ->
 	%UserName = binary:bin_to_list(db_helpers:name_given_id(Id)),
 	UserName = binary:bin_to_list(Name),
 	EmailBody1 = "Hi ",
-	EmailBody2 = "Congratulations, your beta account is now ready to access. You are now one of very few people that have this exclusive privilege. On behalf of the whole team, I welcome you to JoChoice. For now, you can view and vote on photos, but you will soon be able to do more." ,
+	EmailBody2 = "Congratulations, your beta account is now almost ready to access. You are now one of very few people that have this exclusive privilege. On behalf of the whole team, I welcome you to JoChoice. For now, you can view and vote on photos, but you will soon be able to do more." ,
 	EmailBody3 = "Just click the link below, this will activate your account, and let you set your password, so you can login. ",
 	EmailBody4 = "We look forward to seeing you soon at JoChoice.." ,
 	NewLine = "\n",
 	EmailBody5 = "Cheers! \n Jo.",
 	EmailBody = general_helpers:list_merge_no_sort([EmailBody1, UserName, ",", NewLine, NewLine, EmailBody2, NewLine, NewLine, EmailBody3, NewLine, ResetLink, NewLine, NewLine, EmailBody4, NewLine, NewLine, EmailBody5]),
 	%erlang:display(EmailBody),
-	EmailHeader1 = "Subject: Welcome to Jochoice \r\nFrom: JoChoice Host <arunanda1985@gmail.com>\r\nTo: ",
+	EmailHeader1 = "Subject: Welcome to Jochoice \r\nFrom: JoChoice Host <mail@jochoice.com>\r\nTo: ",
 	EmailHeader2 = UserName,
 	EmailHeader3 = "<",
 	EmailHeader4 = UserEmail,
@@ -74,14 +113,19 @@ build_signup_token_email(TokenValue, _Id, UserEmail, Name) ->
 	%erlang:display(EmailHeaders),
 	{EmailHeaders, EmailBody}.
 
-%send website messages
+%send messages from website via contact form to own email address. 
 send_website_message(WebUserName, WebUserEmail, MessageContent) ->
-	Message = general_helpers:list_merge_no_sort(["Sender Name - ", WebUserName, "\n", "Sender Email -  ", WebUserEmail, "\n", MessageContent]),
+	%Message = general_helpers:list_merge_no_sort(["Sender Name - ", WebUserName, "\n", "Sender Email -  ", WebUserEmail, "\n", MessageContent]),
+	Body = general_helpers:list_merge_no_sort(["Sender Name - ", WebUserName, "\n", "Sender Email -  ", WebUserEmail, "\n", MessageContent ]),
+	{Header} = build_message_header(),
+	EmailContent = string:concat(Header, Body),
 	%Message = general_helpers:list_merge_no_sort([ WebUserName, "\n", WebUserEmail, "\n", MessageContent]),
-	erlang:display(Message),
-	Sender = "JoChoice Messenger",
-	ReceiverEmail = <<"shriyogeshchandra@gmail.com">>,
-	send(Sender, ReceiverEmail, Message).
+	%erlang:display(EmailContent),
+	erlang:display(new_user_message_via_contactform),
+	SenderName = "JoChoice Messenger",
+	%ReceiverEmail = <<"contact@jochoice.com">>,
+	ReceiverEmail = <<"mailjochoice@gmail.com">>,
+	send(SenderName, ReceiverEmail, EmailContent).
 
 %send automatic reply to people sending message via the webform. 
 send_website_message_autoreply(SenderName, SenderEmail) ->
@@ -100,7 +144,7 @@ build_website_message_autoreply(SenderName, SenderEmail) ->
 	EmailBody5 = "Cheers!",
 	EmailBody = general_helpers:list_merge_no_sort([EmailBody1, SenderName, ",", NewLine, NewLine, EmailBody2, NewLine, NewLine, EmailBody3, NewLine, NewLine, EmailBody4, NewLine, NewLine, EmailBody5]),
 	%erlang:display(EmailBody),
-	EmailHeader1 = "Subject: Re - Your message to JoChoice \r\nFrom: JoChoice Messenger <arunanda1985@gmail.com>\r\nTo: ",
+	EmailHeader1 = "Subject: Re - Your message to JoChoice \r\nFrom: JoChoice Messenger <mail@jochoice.com>\r\nTo: ",
 	EmailHeader2 = SenderName,
 	EmailHeader3 = "<",
 	EmailHeader4 = SenderEmail,
@@ -108,4 +152,15 @@ build_website_message_autoreply(SenderName, SenderEmail) ->
 	EmailHeaders = general_helpers:list_merge_no_sort([EmailHeader1, EmailHeader2, EmailHeader3, EmailHeader4, EmailHeader5]),
 	%erlang:display(EmailHeaders),
 	{EmailHeaders, EmailBody}.
+
+% create message header for use in send_website_message (to own address) function
+build_message_header() ->
+	EmailHeader1 = "Subject: User message to JoChoice \r\nFrom: JoChoice Messenger <mail@jochoice.com>\r\nTo: ",
+	EmailHeader2 = ["JoChoice Messenger"],
+	EmailHeader3 = "<",
+	EmailHeader4 = ["mail@jochoice.com"],
+	EmailHeader5 = ">\r\n\r\n",
+	EmailHeaders = general_helpers:list_merge_no_sort([EmailHeader1, EmailHeader2, EmailHeader3, EmailHeader4, EmailHeader5]),
+	%erlang:display(EmailHeaders),
+	{EmailHeaders}.
 
